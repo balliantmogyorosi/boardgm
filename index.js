65d9b0c3a7e9ecbd72e826fa4187fcb7f95a85fd
balliantmogyorosi <balliant.mogyorosi@gmail.com>
2021-11-04 10:21:11 +0100

    Heroku init

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('./config/mongoose')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 // Load routes
 const boardGameRoutes = require('./routes/api/boardGameRoutes')
@@ -14,6 +15,16 @@ app.use(bodyParser.json())
 
 // Use routes
 app.use('/api/board-games', boardGameRoutes)
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
