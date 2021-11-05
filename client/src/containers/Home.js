11ac256fafeb209b41e672b8981d7381706e6d8e
balliantmogyorosi <balliant.mogyorosi@gmail.com>
2021-11-05 21:58:19 +0100

    Minimized home content. Changed layout for list

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import NavigationHeader from '../components/Navigation/NavigationHeader/NavigationHeader'
-import BoardGameList from '../components/BoardGam/BoardGameList/BoardGameList'
-import Hero from '../components/Hero/Hero'
+import BoardGameList from '../components/BoardGame/BoardGameList/BoardGameList'
 import { fetchBoardGames } from '../store/actions/boardGamesActions'
-import NavigationFooter from '../components/Navigation/NavigationFooter/NavigationFooter'
 import Container from '../components/Layout/Container/Container'
 
 
@@ -25,27 +22,20 @@ const Home = () => {
         handleFetchBoardGames()
     }, [dispatch])
 
-    console.log(isLoading)
+    if (isLoading) {
+        return (
+            null
+        )
+    }
 
     return (
         <div className="home">
-            <NavigationHeader
-
-            />
-            <Hero
-                backgroundImage="https://images.unsplash.com/photo-1610890716171-6b1bb98ffd09?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1631&q=80"
-                title="Board GM"
-                text="Társasjátékos kávézó"
-            />
             <Container>
                 <BoardGameList
-                    title='Társasjáték kínálatunk'
+                    title='Társasjáték kínálat'
                     boardGameList={boardGameList}
                 />
             </Container>
-            <NavigationFooter
-
-            />
         </div>
     )
 }
